e48f53bc3b4e199eaa43a2ffed895211adf52d1c
Ivan Vorobei <ivanvorobei@icloud.com>
2019-06-07 19:49:25 +0300

    Update Blur and Tranition

---
+++ Examples/Transition and Blur/Basic Animation/SwiftUI-BasicAnimation/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  SwiftUI-BasicAnimation
+//
+//  Created by Meng To on 2019-06-06.
+//  Copyright © 2019 Meng To. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var show = false
+    
+    var body: some View {
+        
+        VStack {
+            Text("Learning SwiftUI")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .color(.black)
+                .padding(4)
+                .animation(.basic(duration: 0.3, curve: .easeOut))
+            
+            Image("ui")
+                .frame(width: show ? 414 : 300, height: show ? 600 : 300)
+                .clipped()
+                .cornerRadius(show ? 0 : 30)
+                .shadow(radius: 30)
+                .blur(radius: show ? 0 : 30)
+                .animation(.spring())
+                .aspectRatio(contentMode: .fill)
+            
+            Text("A course focused on UI")
+                .font(.subheadline)
+                .fontWeight(.regular)
+                .color(.gray)
+                .padding(4)
+                .animation(.basic(duration: 0.4, curve: .easeIn))
+            
+            Button(action: {
+                withAnimation {
+                    self.show.toggle()
+                }
+            }) {
+                Text("Animate")
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .colorScheme(.dark)
+    }
+}
+#endif
---
+++ Examples/Transition and Blur/Card Animation/SwiftUI-CardAnimation/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  SwiftUI-CardAnimation
+//
+//  Created by Meng To on 2019-06-06.
+//  Copyright © 2019 Meng To. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    @State var show = false
+    
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                self.show.toggle()
+            }
+        }) {
+            VStack() {
+                Text("Learn SwiftUI")
+                    .color(.white)
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .padding(.top, show ? 100 : 20)
+                
+                Text("A course on UI and animations")
+                    .color(Color(hue: 0.567, saturation: 0.158, brightness: 0.943))
+                    .lineLimit(-1)
+                
+                Spacer()
+                
+                Text("Card Animation")
+                    .color(Color(hue: 0.498, saturation: 0.609, brightness: 1.0))
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .padding(.bottom, show ? 100 : 20)
+                }
+                .padding()
+                .frame(width: show ? 414 : 300, height: show ? 950 : 300)
+                .background(Color.blue)
+            }
+            .cornerRadius(30)
+            .animation(.spring())
+            .shadow(radius: 30)
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
