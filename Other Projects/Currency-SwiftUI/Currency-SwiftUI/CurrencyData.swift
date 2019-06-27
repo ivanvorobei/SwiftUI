4b241f9e714c777af0c09ad4ecb87975a8a0dd47
Alex Liu <alexliubo@gmail.com>
2019-06-27 02:59:51 -0400

    Added a project.

---
+++ Other Projects/Currency-SwiftUI/Currency-SwiftUI/CurrencyData.swift
@@ -0,0 +1,59 @@
+//
+//  CurrencyData.swift
+//  Currency-SwiftUI
+//
+//  Created by Alex Liu on 2019-06-20.
+//  Copyright © 2018 Alex Liu <alexliubo@gmail.com> All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+import SwiftUI
+
+struct Currency: Equatable, Hashable, Codable, Identifiable{
+    let id: UUID
+    let name: String
+    
+    // This is a rate to USD
+    let rate: Double
+    let symbol: String
+    let code: String
+    
+    init(name: String, rate: Double, symbol: String = "", code: String) {
+        self.id = UUID()
+        self.name = name
+        self.rate = rate
+        self.symbol = symbol
+        self.code = code
+    }
+    
+    // Get unicode flag by currency symbol
+    var flag: String {
+        // Hard-code EU for now
+        if self.symbol == "EU" { return "🇪🇺" }
+        let base : UInt32 = 127397
+        var s = ""
+        for v in self.symbol.uppercased().unicodeScalars {
+            s.unicodeScalars.append(UnicodeScalar(base + v.value)!)
+        }
+        
+        return s
+    }
+}
+
